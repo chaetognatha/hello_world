9921d0948e26a50e6405471c8875bbb7a286d790
Mattis Knulst <mattis.knulst@med.lu.se>
2024-01-04 14:06:22 +0100

    make cli app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    // Create a new clap app
+    let matches = App::new("Interactive hello world cli app")
+    // Add a command line argument
+    .arg(Arg::with_name("say")
+    // Add a help message for the argument
+    .help("What do you want to say?")
+    // Make the argument required
+    .required(true)
+    // Specify that it is the first argument
+    .index(1))
+    // Get the matches
+    .get_matches();
+    // get the value of the say argument
+    let say = matches.value_of("say").unwrap();
+    // Print the value
+    println!("{}", say);
 }
