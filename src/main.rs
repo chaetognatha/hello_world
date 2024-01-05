df3cf2844f3dcf3922e506d6d25f1beb9b4ee276
Mattis Knulst <mattis.knulst@med.lu.se>
2024-01-05 09:45:11 +0100

    add flag for system execution

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use clap::Parser;
+use std::process::Command;
 
-// Parser is a derive macro that generates a struct that implements the clap::Parser trait
-// the Parser trait provides a parse method that parses the cli args
-// Debug is a derive macro that implements the Debug trait for the struct
-// attributes in rust are used to provide metadata about the item they are attached to
+// below are macros, a fast and convenient way to avoid boilerplate
+// I think of them a bit like python decorators
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 // a struct that defines which cli args are expected
@@ -11,12 +10,25 @@ struct Args {
     // attribute that provides info on how to parse the argument
     // in this case you can use the -s or --say flag to pass a string
     #[arg(short, long)]
-    say: String,
+    say: Option<String>,
+    #[arg(short, long)]
+    list: Option<bool>,
 }
 
 fn main() {
     // parse the cli args
     let args = Args::parse();
-    // print the cli args
-    println!("You said: {}", args.say)
-}
+    
+    // check if say is set 
+    if let Some(say) = args.say {
+        // if say is set, print the value
+        println!("You said: {}", say);
+    } else if args.list.is_some() {
+        let result = Command::new("ls")
+            .arg("-l")
+            .output()
+            .expect("failed to execute process");
+        println!("{}", String::from_utf8_lossy(&result.stdout));
+    }
+    }
+    
